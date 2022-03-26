2fd4e68df2a9b5352ec4331b4b56d5ed98216371
tfunato <tfunato@gmail.com>
2022-03-26 18:11:59 +0900

    feat: :sparkles: add users base

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
   },
 };
