2e18183c03da4317d866809f4fa35335c3a3ff32
tfunato <tfunato@gmail.com>
2022-04-11 22:23:22 +0900

    chor: :ticket: fixed lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,12 +19,12 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'semi': ['error', 'never', {'beforeStatementContinuationChars': 'never'}],
-    'semi-spacing': ['error', {'after': true, 'before': false}],
+    '@typescript-eslint/no-explicit-any': 'error',
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    'semi-spacing': ['error', { after: true, before: false }],
     'semi-style': ['error', 'first'],
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error'
+    'no-unreachable': 'error',
   },
-};
+}
